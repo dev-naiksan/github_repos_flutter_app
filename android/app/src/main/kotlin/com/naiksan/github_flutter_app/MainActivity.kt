996701e313bee1a6bdb572da4286c0796ae0824b
Sanket Naik <dev@naiksan.com>
2021-11-24 19:21:33 +0530

    Added biometric authentication

--- android/app/src/main/kotlin/com/naiksan/github_flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/naiksan/github_flutter_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.naiksan.github_flutter_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:github_flutter_app/ui/auth/auth_screen.dart';
+import 'package:local_auth/local_auth.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String routeName = '/';
@@ -10,12 +12,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  final LocalAuthentication auth = LocalAuthentication();
+  bool _authFailed = false;
+
   @override
   void initState() {
     super.initState();
     SchedulerBinding.instance?.addPostFrameCallback((_) async {
-      await Future.delayed(Duration(seconds: 2));
-      _navigate();
+      await Future.delayed(Duration(seconds: 1));
+      _authenticate();
     });
   }
 
@@ -23,12 +28,42 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          child: Text('Splash'),
-          onPressed: () {},
+        child: Visibility(
+          visible: _authFailed,
+          child: TextButton(
+            child: Text("Retry"),
+            onPressed: () {
+              _authenticate();
+            },
+          ),
         ),
       ),
     );
+  }
+
+  void _authenticate() async {
+    final authenticated = await _authenticateWithBiometrics();
+    if (authenticated) {
+      _navigate();
+    } else {
+      setState(() {
+        _authFailed = true;
+      });
+    }
+  }
+
+  Future<bool> _authenticateWithBiometrics() async {
+    try {
+      return await auth.authenticate(
+        localizedReason: 'Scan your fingerprint to authenticate',
+        useErrorDialogs: true,
+        stickyAuth: true,
+        biometricOnly: true,
+      );
+    } on PlatformException catch (e) {
+      print(e);
+      return false;
+    }
   }
 
   void _navigate() async {
